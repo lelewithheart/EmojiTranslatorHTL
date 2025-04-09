5d8e43bc28bd8d4e5d92c86dce96af2b7b1f4303
lelewithheart <115990008+lelewithheart@users.noreply.github.com>
2025-04-09 12:07:17 +0200

    unit Tests gehn Eier Leckn

--- EmojiTests/UnitTest1.cs
+++ EmojiTests/UnitTest1.cs
@@ -1,15 +1,52 @@
-namespace EmojiTests;
+using NUnit.Framework;
+using EmojiTranslatorSimple;
 
-public class Tests
+namespace EmojiTests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        private Utils _utils;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void Setup()
+        {
+            _utils = new Utils();
+            _utils.CreateEmojis();
+        }
+
+        [Test]
+        public void TranslateEmoji_KnownWord_ReturnsCorrectEmoji()
+        {
+            string result = _utils.TranslateEmoji("happy");
+            Assert.AreEqual("😄", result);
+        }
+
+        [Test]
+        public void TranslateEmoji_KnownWordWithUpperCase_ReturnsCorrectEmoji()
+        {
+            string result = _utils.TranslateEmoji("Happy");
+            Assert.AreEqual("😄", result);
+        }
+
+        [Test]
+        public void TranslateEmoji_UnknownWord_ReturnsNull()
+        {
+            string result = _utils.TranslateEmoji("unknown");
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void TranslateEmoji_SpecialEntryChickenJockey_ReturnsCorrectSequence()
+        {
+            string result = _utils.TranslateEmoji("chicken jockey");
+            Assert.AreEqual("👶🐔🧟", result);
+        }
+
+        [Test]
+        public void OutputEmojis_DoesNotThrow()
+        {
+            // We are just testing that it executes without exceptions.
+            Assert.DoesNotThrow(() => _utils.OutputEmojis());
+        }
     }
 }
--- EmojiTranslatorSimple/obj/Debug/net8.0/EmojiTranslatorSimple.AssemblyInfo.cs
+++ EmojiTranslatorSimple/obj/Debug/net8.0/EmojiTranslatorSimple.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmojiTranslatorSimple")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5beaa13a75efa020a1a1aa9deee6898efb9740d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmojiTranslatorSimple")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmojiTranslatorSimple")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
