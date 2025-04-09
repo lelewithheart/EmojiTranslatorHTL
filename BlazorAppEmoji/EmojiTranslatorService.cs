de42066464490af6b2f6e093abb011904d56acde
David <d.thielmann@htlkrems.at>
2025-04-09 12:03:57 +0200

    Beschde wo gibt Blazor

---
+++ BlazorAppEmoji/EmojiTranslatorService.cs
@@ -0,0 +1,24 @@
+﻿using EmojiTranslatorSimple;
+
+namespace BlazorAppEmoji;
+
+public class EmojiTranslatorService
+{
+    private readonly Utils _utils = new();
+
+    public EmojiTranslatorService()
+    {
+        _utils.CreateEmojis();
+    }
+
+    public string? Translate(string input)
+    {
+        return _utils.TranslateEmoji(input);
+    }
+
+    public Dictionary<string, string> GetAll()
+    {
+        var field = typeof(Utils).GetField("_emojiDictionary", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+        return field?.GetValue(null) as Dictionary<string, string> ?? new();
+    }
+}
